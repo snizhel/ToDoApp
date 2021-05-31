bedbba38898e8c16660fef5c7731c5b5864b3330
Thien <bananakind123@gmail.com>
2021-05-31 15:26:31 +0700

    change loginDirect to popup

--- new(socket.io)/my-app/src/components/LoginButton.js
+++ new(socket.io)/my-app/src/components/LoginButton.js
@@ -3,9 +3,9 @@ import { useAuth0 } from '@auth0/auth0-react'
 import IconButton from '@material-ui/core/IconButton';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithPopup } = useAuth0();
     return (
-        <IconButton onClick={() => loginWithRedirect()}>
+        <IconButton onClick={() => loginWithPopup()}>
             <AccountCircleIcon />
         </IconButton>
     )
